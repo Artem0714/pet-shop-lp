9443c628b2f936288ab3ef18fb0315530245f159
Artem0714 <arfed96@gmail.com>
2023-02-02 14:42:42 +0300

    refactor(css):included fonts in the App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='test'>Hello</p>
+      <p className='test2'>Hello</p>
+    </div>
+  );
+}
+
+export default App;
